924f27294aa29af9d915e3d5ac3d4a97820f09ff
juliositgespomares <juliositgespomares@penguin>
2022-10-04 06:35:53 -0500

    Updates

---
+++ precache-manifest.cc08c0e0dc7180e4655377af43e9502f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffba3c6e25d8241cb39aa27610eb832",
+    "url": "/servitodo/index.html"
+  },
+  {
+    "revision": "f9b93638eb10b3797531",
+    "url": "/servitodo/static/css/main.7b7a544b.chunk.css"
+  },
+  {
+    "revision": "54410a88ba1b46f3b043",
+    "url": "/servitodo/static/js/2.c906c940.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/servitodo/static/js/2.c906c940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b93638eb10b3797531",
+    "url": "/servitodo/static/js/main.fe0edcad.chunk.js"
+  },
+  {
+    "revision": "a6bd01e8f54de33b454e",
+    "url": "/servitodo/static/js/runtime-main.54af1fbb.js"
+  },
+  {
+    "revision": "6cd1eae440ef34a56195965c8986629e",
+    "url": "/servitodo/static/media/logo.6cd1eae4.png"
+  },
+  {
+    "revision": "93f33577a4098c59ab1e7d5f974135b4",
+    "url": "/servitodo/static/media/profilePic.93f33577.jpeg"
+  }
+]);
